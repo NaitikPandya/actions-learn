bcdc3130b16b1b58481cc13c101d5373874c7e68
NaitikPandya <naitikpandya40@gmail.com>
2023-12-17 20:48:03 +0530

    we

--- react-webapp/src/App.js
+++ react-webapp/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
      <div className='AddItem'>
-      <TextField value={enteredText} label="Item" inputProps={{
+      <TextField value={enteredText} label="Item1" inputProps={{
         'data-testid': 'itemTextInput'
       }} data-testid='itemTextf' onChange={(value) => {
         if(value.target.value){
